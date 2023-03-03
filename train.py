c570df1a123dd6812481c640576e7e2f0f2a20c2
shirinpirouzkhaah <63603035+shirinpirouzkhaah@users.noreply.github.com>
2023-03-03 15:36:49 +0100

    Update train.py

--- train.py
+++ train.py
@@ -86,16 +86,7 @@ def main():
     f_sh.close()
     logging.info("train_model.sh")
     
-    
-    # simulate training
-    epochs = 10
-    offset = random.random() / 5
-    for epoch in range(2, epochs):
-        acc = 1 - 2 ** -epoch - random.random() / epoch - offset
-        loss = 2 ** -epoch + random.random() / epoch + offset
-        
-        # log metrics to wandb
-        wandb.log({"acc": acc, "loss": loss})
+   
         
     # finish the wandb run
     wandb.finish()
